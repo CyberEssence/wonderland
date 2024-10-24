9e7542bf56799d523df062a705b8779e02d5b11e
ultrageeek <41297691+CyberEssence@users.noreply.github.com>
2024-10-24 18:55:45 +0300

    Update to Mutated Hash Algorithm (MHA-4473)

--- diffi-halman.c
+++ diffi-halman.c
@@ -1,100 +1,54 @@
-#include <stdio.h>
-#include <time.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <cmath>
+#include <sstream>
+#include <climits>
+#include <cstring>
+#include <bitset>
+
+using namespace std;
 
 #define MAX 1024
 
-char *gen_hash(char *sentence);
-void gen_random_string(char *dest, int string_length);
-char *random_permutations(char *str);
-void swap(char *x, char *y);
-int super_tetration(int g, int a, int z);
-void clean_from_spaces(char *sentence);
-//unsigned sqrt_cpu_int(unsigned long L);
-void concat_numbers(int *arr, size_t size, char *result, size_t result_size);
-int root(int input, int n);
-int power(int g, int a);
-void to_int_array(int count, int num);
-
-int main() {
-	int p = 23;
-	int g = 5;
-	char sentence[256];
-	char clean_sentence[256];
-	printf("Введите строку: ");
-        scanf("%249[^\n]", &sentence);  
-        
-        
-        char *cleaned_text = gen_hash(sentence);
-        clean_from_spaces(cleaned_text);
-        printf("%s\n", cleaned_text);
-        int hash_size = strlen(cleaned_text);
-        int int_hash;
-        
-        printf("%d", cleaned_text);
-        
-        int sum = 0;
-        for (int i = 0; i < hash_size; i++) {
-            int_hash = (int)cleaned_text[i];
-        }
-        
-        int power = (int)int_hash;
-        
-        printf("%d ", (int)int_hash);
-        
-        
-        printf("%d ", super_tetration(g, root(power, 5), p));
-        int hashed = super_tetration(g, root(power, 5), p);
-        
-        
-        int num = count_digits(hashed);     
-        to_int_array(num, hashed);
-
-        return 0;
-}
-
-// Function to free the allocated memory
-void free_array(int* arr) {
-    free(arr);
-}
-
-int count_digits(int n) {
-    // For digit 0
-    if(n == 0)
-          return 1;
-      
-    int count = 0;
-
-    // Remove last digit from number
-    // till number is 0
-    while (n != 0) {
-
-        // Increment count
-        count++;
-        n /= 10;
+// Функция для генерации случайной строки
+string genRandomString(int length) {
+    const string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(0, characters.size() - 1);
+    
+    string result(length, ' ');
+    for (int i = 0; i < length; ++i) {
+        result[i] = characters[dis(gen)];
     }
-
-    // return the count of digit
-    return count;
+    return result;
 }
 
-void to_int_array(int count, int num) {
-    char *str_hashed;
-    int digits[count];
-    int i = count - 1;
-    printf(" ");
-    while (num > 0)
-    {
-        int digit = num % 10;
-        num /= 10;
-        digits[i] = digit;
-        digits[i] += 97;
-        printf("%c", digits[i]);  
-        i--;
-   }
-   printf("\n"); 
+// Функция для генерации хеша
+string mutateAndHash(const string& input) {
+    vector<char> shuffledInput(input.begin(), input.end());
+    shuffle(shuffledInput.begin(), shuffledInput.end(), default_random_engine());
+    
+    string hashString;
+    for (char c : shuffledInput) {
+        if (isalnum(c)) {
+            hashString += c;
+        }
+    }
+    
+    return hashString;
+}
+
+int power(int base, int a) {
+       int production = 1;
+       
+       for (int i = 0; i < a; i++) {
+              production *= base;
+       }
+       return production;
 }
 
 int root(int input, int n)
@@ -102,130 +56,167 @@ int root(int input, int n)
     return round(pow(input, 1./n));
 }
 
-void concat_numbers(int *arr, size_t size, char *result, size_t result_size) {
-    size_t pos = 0;
-    for (size_t i = 0; i < size; i++) {
-        int n = snprintf(result + pos, result_size - pos, "%d", arr[i]);
-        if (n < 0) {
-            fprintf(stderr, "Error formatting number %d", arr[i]);
-            return;
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Функция для очистки строк от пробелов
+string cleanFromSpaces(const string& input) {
+    string result;
+    bool prevSpace = false;
+    
+    for (char c : input) {
+        if (!isspace(c)) {
+            if (!prevSpace) {
+                result += c;
+            }
+            prevSpace = false;
+        } else {
+            prevSpace = true;
         }
-        pos += n;
     }
+    
+    return result;
 }
 
-unsigned sqrt_cpu_int(unsigned long L) {
-        unsigned div = 1, rslt = 0; 
-	while (L > 0)
-	{
-	    L -= div,  div += 2; 
-		rslt += L < 0 ? 0 : 1; 
-	}
-	return rslt; 
+// Функция для генерации хеша из строки
+string genHash(const string& input) {
+    string cleanedInput = cleanFromSpaces(input);
+    string randomString = genRandomString(24);
+    string output = cleanedInput + randomString;
+    return mutateAndHash(output);
 }
 
-int power(int g, int a) {
-       int production = 1;
-       
-       for (int i = 0; i < a; i++) {
-              production *= g;
-       }
-       return production;
+// Функция для генерации случайного числа
+int generateRandomNumber() {
+    random_device rd;
+    static mt19937 gen(rd());
+    uniform_int_distribution<> dis(10000, 99999);
+    return dis(gen);
 }
 
-int super_tetration(int g, int a, int z) {
-       a = power(g, a);
-       int super_production = 1;
-       
-       for (int j = 0; j < a; j++) {
-             super_production *= g;
-       }
-       return super_production;
+// Функция для преобразования строки в массив ASCII-кодов
+std::vector<int> stringToASCIIArray(const std::string& str) {
+    std::vector<int> asciiArray;
+    for (char c : str) {
+        asciiArray.push_back(static_cast<int>(c));
+    }
+    return asciiArray;
 }
 
-void clean_from_spaces(char *sentence) {
-      char res[512] = "";
-      
-      int j = 0;
-      int i;
-      for (i = 0; i < strlen(sentence) ; i++) {
-          if (sentence[i] == ' ') 
-          {
-            if (j==0) continue;
-            if (sentence[i+1] == ' ') continue;
-          }
-          res[j] = sentence[i];
-          j++;
-      }
-      i = strlen(res);
-      if(res[i-2] == ' ')
-          res[i-2] = '\0';
-          
-      return res;
+int concatenateArr(std::vector<int>& arr) {
+    // Сначала преобразуем вектор в строку
+    stringstream ss; 
+    std::string str;
+    int num;
+    for (int num : arr) {
+        str += std::to_string(num);
+    }
+    ss << str;
+    ss >> num;
+
+    //print to the consloe
+    cout << num << endl;
+    return num;
 }
 
-char *gen_hash(char *sentence) {
-        char rand_str[512];     // Buffer for the random string
+std::string xor_strings(const std::string& str1, const std::string& str2) {
+    int len = std::max(str1.length(), str2.length());
+    
+    std::string result;
+    result.reserve(len);
 
-        // Remove spaces from the sentence
-        char *src = sentence, *dst = sentence;
-        while (*src != '\0') {
-            if (*src != ' ') {
-                *dst++ = *src;
-            }
-            src++;
-        }
-        *dst = '\0'; // Null-terminate the new string without spaces
-
-        gen_random_string(rand_str, 24); // Generate a random string
-
-        // Create a new string for the output
-        char *output; // Static to return from the function safely
-        output = strcat(sentence, rand_str);
-        //snprintf(output, sizeof(output), "RandomString: %s, Original: %s", rand_str, sentence);
+    for (int i = 0; i < len; ++i) {
+        char c1 = (i < str1.length()) ? str1[i] : '0';
+        char c2 = (i < str2.length()) ? str2[i] : '0';
         
-        return random_permutations(output);
+        result += ((c1 - '0') ^ (c2 - '0')) + '0';
+    }
+
+    return result;
 }
 
-void swap(char *x, char *y) {
-        char temp;
-        temp = *x;
-        *x = *y;
-        *y = temp;
+int main() {
+    int p = 3;
+    int g = 2;
+    string sentence;
+    cout << "Введите строку: ";
+    getline(cin, sentence); // Используем getline для чтения всей строки
+    
+    string hashedSentence = genHash(sentence);
+    cout << "Хеш: " << hashedSentence << endl;
+    
+    std::vector<int> asciiArray = stringToASCIIArray(hashedSentence);
+    
+    std::cout << "ASCII codes of the string: ";
+    for (int code : asciiArray) {
+        std::cout << code << " ";
+    }
+    std::cout << std::endl;
+    unsigned dataArraySize = sizeof(asciiArray) / sizeof(int);
+    int num = concatenateArr(asciiArray);
+    
+    int x = root(num, 9);
+    
+    cout << root(num, 9);
+    
+    int superTetrationResult = power(3, power(x, 2));
+    
+    cout << "Супертетрация: " << power(3, power(x, 2)) << endl;
+    
+    vector<int> digits;
+    int temp = superTetrationResult;
+    while (temp > 0) {
+        digits.push_back(temp % 10);
+        temp /= 10;
+    }
+    
+    reverse(digits.begin(), digits.end());
+    
+    string result;
+    for (int digit : digits) {
+        result += char(digit + 97); // Преобразование чисел в символы
+    }
+    
+    cout << "Результат: " << result << endl;
+    
+    int j = 0;
+    
+    string array = mutateAndHash(result);
+    
+    string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
+    
+    cout << characters.size();
+    
+    string chars = mutateAndHash(characters).substr(0, array.length());
+    
+    cout << chars << endl;
+    
+    string new_chars;
+    
+    string new_string;
+    
+    string new_second_string;
+    
+    for (int i = 0; i <= characters.size(); i++) {
+        new_chars += mutateAndHash(array + characters);
+        new_string = mutateAndHash(new_chars);
+    }
+    cout << new_string.size() << endl;
+    
+    for (int j = 0; j <= new_string.size() / characters.size(); j++) {
+        new_second_string += characters;
+    }
+    
+    string new_third_string = mutateAndHash(new_second_string.substr(0, new_string.length()));
+    
+    cout << new_third_string.size() << endl;
+    
+    std::string res = xor_strings(new_third_string, new_second_string);
+    cout << mutateAndHash(res) << endl;
+    
+    return 0;
 }
 
-char *random_permutations(char *str) {
-        //char str[100];
-        //printf("Enter a word: ");
-        //scanf("%s", str); // Read input word
-
-        int n = strlen(str);
-        
-        // Seed the random number generator
-        srand(time(0));
-
-        // Generate random permutations
-        printf("Random permutations of the word:\n");
-        for (int i = 0; i < 10; i++) { // Print 10 random permutations
-            // Shuffle the string randomly using Fisher-Yates Algorithm
-            for (int j = n - 1; j > 0; j--) {
-                int k = rand() % (j + 1);
-                swap(&str[j], &str[k]);
-            }
-            //printf("%s", str); // Print the shuffled string
-        }
-
-        return str;
-}
-
-void gen_random_string(char *dest, int string_length) {
-        const char characters[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
-        int num_chars = sizeof(characters) - 1;
-	
-	
-	//scanf("%249s", &string_length);
-        for (int i = 0; i < string_length; ++i) {
-                dest[i] = characters[rand() % num_chars]; // starting on '0', ending on '}'
-        }
-        dest[string_length] = '\0'; 
-}
