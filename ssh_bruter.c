b7b301be2750371c0f81cdd4aba386817c6297ea
ultrageeek <41297691+CyberEssence@users.noreply.github.com>
2024-11-27 02:45:14 +0000

    Update main

--- ssh_bruter.c
+++ ssh_bruter.c
@@ -1,28 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <libssh/libssh.h>
 #include <libssh/callbacks.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include "net/ssh.c"
-#include "net/auth.c"
+#include "net/spider.c"
+#include "net/ssh_auth.c"
 
 int main(int argc, char *argv[]) {
+    /*if (argc != 4) {
+        fprintf(stderr, "Usage: %s <hostname> <start_port> <end_port>\n", argv[0]);
+        return 1;
+    }*/
     
+    
+    
+    char hostname[256];
+    char username[256];
+    int start_port;
+    int end_port;
+    
+    printf("Enter hostname and username: ");
+    scanf("%s %s", &hostname, &username);
+    
+    printf("Enter start port number: ");
+    scanf("%d", &start_port);
+
+    printf("Enter end port number: ");
+    scanf("%d", &end_port);
+    
+
+    struct sockaddr_in addr;
+    if (initialize_socket_address(&addr, hostname) != 0) {
+        fprintf(stderr, "Failed to initialize socket address.\n");
+        exit(2);
+    }
+     
+    port_scan(&addr, start_port, end_port);
+
     ssh_session session;
     ssh_channel channel;
-    int rc, port = 22;
-    char buffer[1024];
+    int rc;
+    char *buffer = malloc(sizeof(char) * 1024); // Allocate memory for buffer
     unsigned int nbytes;
-
     char *password;
-    
-    
-    
+
+    printf("Session...\n");
     printf("Session...\n");
     session = ssh_new();
     if (session == NULL) exit(-1);
 
-    ssh_opt_set(session, port);
+    ssh_options_set(session, SSH_OPTIONS_HOST, hostname);
+    ssh_options_set(session, SSH_OPTIONS_PORT, &start_port);
+    ssh_options_set(session, SSH_OPTIONS_USER, username);
 
     printf("Connecting...\n");
     rc = ssh_connect(session);
@@ -83,10 +112,10 @@ int main(int argc, char *argv[]) {
 
     ssh_free(session);
     
-    ssh_exec_bash(nbytes, rc, channel, session, session);
+    ssh_exec_bash(nbytes, rc, channel, session, buffer);
 
     free_channel(channel);
     free_session(session);
-   
+    
     return 0;
 }
